799e708678dfe1028ff6235bac9f3d56a0ef8d47
theperkinrex <theperkinrex@gmail.com>
2019-11-11 17:38:24 +0100

    Fix linting issues

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import 'source-map-support/register'
 
 import { Memory, CustomMemory } from './components/memory'
 import { ConsoleIO } from './components/io'
-import { readFileSync, exists } from 'fs'
+import { readFileSync } from 'fs'
 import { MINST_LENGTH, MINST_COUNTER_LENGTH, INST_LENGHT, HLT, ICA, ICO1, ICO2, ICI2, ICI1, A1I, A2I, DAI, DAO, IRI, R1I, R1O, R2I, R2O } from './@types/instructions'
 import { AddressSelector, Bus_AddressSelector_Interface } from './components/addressSelector' // eslint-disable-line @typescript-eslint/camelcase
 import { Bus } from './components/bus'
 import { BusRegister } from './components/busRegister'
-import { InstructionCounter, Bus_InstructionCounter_Interface } from './components/instructionCounter'
+import { InstructionCounter, Bus_InstructionCounter_Interface } from './components/instructionCounter' // eslint-disable-line @typescript-eslint/camelcase
+
+// BIG ENDIAN SYSTEM
+
+const DEBUG = false
 
 /* Address sections (16bit/2Byte address)
 * 0x0000 - 0x7FFF: 32kB - RAM
@@ -35,7 +39,7 @@ let io = new ConsoleIO(4) // 16B - 0x8010 - 0x801F
 ram.hexdump()
 console.log()
 console.log()
-//rom.hexdump()
+// rom.hexdump()
 
 let addressSelector = new AddressSelector(16)
 
@@ -68,7 +72,7 @@ bus.addBusIO(privateRegister2)
 let haltFlag = false
 
 // #region Microinstructions
-
+/* eslint-disable @typescript-eslint/camelcase */
 // TODO: ACI, ACO, ADD
 
 function f_ICO1 () {
@@ -131,91 +135,92 @@ function f_HLT () {
 	haltFlag = true
 }
 
+/* eslint-enable @typescript-eslint/camelcase */
 // #endregion Microinstructions
-//let j = 0
+// let j = 0
 
 while (!haltFlag) {
 	// Read instruction -> go trough microinstructions -> start over
-  let instruction = instructionRegister.getVal()
-  //console.log('#',instructionCounter.counter.toString(16).padStart(4,'0'),instruction.toString(16).padStart(2,'0'))
+	let instruction = instructionRegister.getVal()
+	if (DEBUG) console.log('#', instructionCounter.counter.toString(16).padStart(4, '0'), instruction.toString(16).padStart(2, '0'))
 	for (let i = 0; i < Math.pow(2, MINST_COUNTER_LENGTH); i++) {
 		let romAddress = (instruction << MINST_COUNTER_LENGTH) + i
-    let romVal = rom.get(romAddress)
-    if(romVal == 0) break
-    let mcodes = ''
+		let romVal = rom.get(romAddress)
+		if (romVal === 0) break // break out of the loop if no microcode
+		let mcodes = ''
 		if ((romVal & HLT) !== 0) {
-      f_HLT()
-      mcodes += 'HLT '
+			f_HLT()
+			mcodes += 'HLT '
 		}
 		if ((romVal & ICA) !== 0) {
-      f_ICA()
-      mcodes += 'ICA '
+			f_ICA()
+			mcodes += 'ICA '
 		}
 		if ((romVal & ICO1) !== 0) {
-      f_ICO1()
-      mcodes += 'ICO1 '
+			f_ICO1()
+			mcodes += 'ICO1 '
 		}
 		if ((romVal & ICO2) !== 0) {
-      f_ICO2()
-      mcodes += 'ICO2 '
+			f_ICO2()
+			mcodes += 'ICO2 '
 		}
 		if ((romVal & ICI1) !== 0) {
-      f_ICI1()
-      mcodes += 'ICI1 '
+			f_ICI1()
+			mcodes += 'ICI1 '
 		}
 		if ((romVal & ICI2) !== 0) {
-      f_ICI2()
-      mcodes += 'ICI2 '
+			f_ICI2()
+			mcodes += 'ICI2 '
 		}
 
 		if ((romVal & A1I) !== 0) {
-      f_A1I()
-      mcodes += 'A1I '
+			f_A1I()
+			mcodes += 'A1I '
 		}
 		if ((romVal & A2I) !== 0) {
-      f_A2I()
-      mcodes += 'A2I '
+			f_A2I()
+			mcodes += 'A2I '
 		}
 
 		if ((romVal & DAI) !== 0) {
-      f_DAI()
-      mcodes += 'DAI '
+			f_DAI()
+			mcodes += 'DAI '
 		}
 		if ((romVal & DAO) !== 0) {
-      f_DAO()
-      mcodes += 'DAO '
+			f_DAO()
+			mcodes += 'DAO '
 		}
 
 		if ((romVal & IRI) !== 0) {
-      f_IRI()
-      mcodes += 'IRI '
+			f_IRI()
+			mcodes += 'IRI '
 		}
 
 		if ((romVal & R1I) !== 0) {
-      f_R1I()
-      mcodes += 'R1I '
+			f_R1I()
+			mcodes += 'R1I '
 		}
 		if ((romVal & R1O) !== 0) {
-      f_R1O()
-      mcodes += 'R1O '
-    }
-    
-    if ((romVal & R2I) !== 0) {
-      f_R2I()
-      mcodes += 'R2I '
+			f_R1O()
+			mcodes += 'R1O '
+		}
+
+		if ((romVal & R2I) !== 0) {
+			f_R2I()
+			mcodes += 'R2I '
 		}
 		if ((romVal & R2O) !== 0) {
-      f_R2O()
-      mcodes += 'R2O '
-    }
-    //console.log('-', romVal.toString(2).padStart(MINST_LENGTH, '0'), romVal.toString(16), mcodes)
+			f_R2O()
+			mcodes += 'R2O '
+		}
+		if (DEBUG) console.log('-', romVal.toString(2).padStart(MINST_LENGTH, '0'), romVal.toString(16), mcodes)
 		bus.cycle()
-  }
-  /*if(j==4){
+	}
+	/* if(j==4){
     console.log('j broke')
     break
   }
-  j++*/
+  j++ */
 }
 
 console.log()
--- src/ram.ts
+++ src/ram.ts
@@ -4,28 +4,28 @@ import { Memory } from './components/memory'
 import { createBinFile } from './utils/bin'
 import { inst as i } from './@types/instructions'
 
-// Little-endian? big part first, small part last
-
 let ram = new Memory(15) // first bit to select ram or another thing
 for (let idx = 0x00; idx < 0x8000; idx++) { // Empty it
 	ram.set(idx, 0x00)
 }
 
 let code: {[address: number]: number} = {
-	0x00: i.MOV.code,
-	0x01: 0x00,
-	0x02: 0x0B,
-	0x03: 0x80,
-	0x04: 0x10,
-	0x05: i.MOV.code,
-	0x06: 0x00,
-	0x07: 0x0C,
-	0x08: 0x80,
-	0x09: 0x11,
-	0x0A: i.HALT.code,
-	0x0B: 70, // F
-	0x0C: 65, // A
+	0x0000: i.MOV.code,
+	0x0001: 0x00,
+	0x0002: 0x0B,
+	0x0003: 0x80,
+	0x0004: 0x10,
+	0x0005: i.MOV.code,
+	0x0006: 0x00,
+	0x0007: 0x0C,
+	0x0008: 0x80,
+	0x0009: 0x11,
+	0x000A: i.HALT.code,
+	0x000B: 70, // F
+	0x000C: 65 // A
 }
+
+// TODO: write real compiler
 
 for (let address in code) {
 	ram.set(parseInt(address), code[address])
